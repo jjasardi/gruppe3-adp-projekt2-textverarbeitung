ad928c256706bf24c844b878738926f852340409
Durim Sadiku <sadikdur@students.zhaw.ch>
2021-11-04 21:18:41 +0100

    javadoc + ergänzung

--- src/Format.java
+++ src/Format.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
-
+/**
+ * Diese Klasse enthält methodem zur Formatierung von Absätzen.
+ * 
+ * @author (sadikdur) 
+ * @version (1)
+ */
 public class Format {
     private ArrayList<String> testText;
     private String text;
@@ -21,20 +26,46 @@ public class Format {
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry");
     }
 
-
+    /**
+     * Prüft ob Indexeingabe Valide ist und fügt Zeilenumbrüche nach definierter
+     * maximalen Spaltenlänge.
+     * 
+     * @param width definiert maximale Spaltenlänge
+     * @param index Position der Sammlung
+     */
     public void formatFix(int width, int index) {
-        format4 = testText.get(index).split(" ");
-        int chars = 0;
-        for (int slot = 0; slot < format4.length; slot++) {
-            chars = chars + format4[slot].length();
-                if (chars < width) {
-                    format4[slot] = format4[slot] + " ";
-                } else {
-                    format4[slot] = format4[slot] + "\n";
-                    chars = 0;
-                }
-        }   
-        text = index + ":" + String.join("", format4);
+        if (index < 0 || index >= testText.size()) {
+            System.err.println("Index out of Bounds");
+        } else {
+            format4 = testText.get(index).split(" ");
+            int chars = 0;
+            for (int slot = 0; slot < format4.length; slot++) {
+                chars = chars + format4[slot].length();
+                if (slot < format4.length - 1)
+                    if (chars + format4[slot + 1].length() < width) {
+                        format4[slot] = format4[slot] + " ";
+                    } else {
+                        format4[slot] = format4[slot] + " \n";
+                        chars = 0;
+                    }
+            }
+        }
+        text = String.join("", format4);
         System.out.println(text);
+    }
+
+    /**
+     * Prüft ob Indexeingabe Valide ist und fügt Nummer zum Absatz hinzu. Format:
+     * <n> : <Absatz>
+     * 
+     * @param index Position der Sammlung
+     */
+    public void formatRaw(int index) {
+        if (index < 0 || index >= testText.size()) {
+            System.err.println("Index out of Bounds");
+        } else {
+            text = (index + 1) + " : " + testText.get(index);
+            System.out.println(text);
+        }
     }
 }
