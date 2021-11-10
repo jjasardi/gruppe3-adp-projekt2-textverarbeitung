a63b339430db0f947b1498c1bf7e07e5b391ab28
Durim Sadiku <sadikdur@students.zhaw.ch>
2021-11-10 08:21:43 +0100

    entkopplung

--- src/Output.java
+++ src/Output.java
@@ -1,11 +1,17 @@
 import java.util.HashMap;
 
 public class Output {
+    Logic logic;
+    Input input;
 
     private HashMap<String, String> output;
     private HashMap<String, String> errorOutput;
 
     public Output() {
+        logic = new Logic(this);
+        input = new Input(this);
+        input.setLogic(logic);
+        logic.setInput(input);
         output = new HashMap<>();
         errorOutput = new HashMap<>();
         setOutput();
@@ -18,16 +24,16 @@ public class Output {
                 + "Bitte geben Sie Einen Text ein oder erstellen "
                 + "sie einen Dummy-Text mit dem Befehl DUMMY. \n");
         output.put("help", "HELP"); //TODO: einen Help erstellen.
-        output.put("command", "Bitte geben Sie einen Befehl ein.\n>");
-        output.put("addText", "Bitte geben Sie einen Text ein.\n>");
+        output.put("command", "Bitte geben Sie einen Befehl ein.\n> ");
+        output.put("addText", "Bitte geben Sie einen Text ein.\n> ");
         output.put("addedText", "Ihr Text wurde hinzugefügt.\n");
         output.put("addedTextn", "Ihr Text wurde zu diesem Index hinzugefügt: ");
         output.put("addedDummy", "Ein Dummy-Text wurde hinzugefügt.\n");
         output.put("addedDummyn", "Ein Dummy-Text zu diesem Index hinzugefügt: ");
         output.put("toRaw", "Das Format ist jetzt auf Standart eingestellt.\n");
         output.put("toFix", "Das Format ist jetzt auf diesen Wert eingestellt: ");
-        output.put("replace", "Welches Wort möchten Sie ersetzen?\n>");
-        output.put("toReplace", "Mit welchem Wort möchten Sie Ihre Auswahl ersetzen?\n>");
+        output.put("replace", "Welches Wort möchten Sie ersetzen?\n> ");
+        output.put("toReplace", "Mit welchem Wort möchten Sie Ihre Auswahl ersetzen?\n> ");
         output.put("del", "Der letzte Absatz wurde gelöscht.\n");
         output.put("deln", "Dieser Absatz wurde gelöscht: ");
     }
@@ -50,4 +56,13 @@ public class Output {
     public String getErrorOutput(String key) {
         return errorOutput.get(key);
     }
+
+    public boolean run() {
+        return logic.run();
+    }
+
+    public void formatNextLine() {
+        input.formatNextLine();
+    }
+
 }
--- src/Texteditor.java
+++ src/Texteditor.java
@@ -1,6 +1,4 @@
 public class Texteditor {
-    private Input input;
-    private Logic logic;
     private Output output;
 
     public static void main(String[] args) {
@@ -14,17 +12,10 @@ public class Texteditor {
 
     public void start() {       
         output = new Output();
-        logic = new Logic(output);
-        input = new Input(output);      
-        logic.setInput(input);
-        input.setLogic(logic);
         System.out.print(output.getOutput("welcome"));
-        while (logic.run() == true) {
+        while (output.run() == true) {
         System.out.print(output.getOutput("command"));
-        input.formatNextLine();
-
-        System.out.println(input.getCommand());
-        System.out.println(input.getParagraph());
+        output.formatNextLine();
         }
     }
 }
